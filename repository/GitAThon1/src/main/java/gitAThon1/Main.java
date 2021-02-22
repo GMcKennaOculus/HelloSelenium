7b726a5c0886582062c9c56cbef9bab829b56994
GMcKennaOculus <79461528+GMcKennaOculus@users.noreply.github.com>
2021-02-22 13:18:39 -0500

    Add files via upload

---
+++ repository/GitAThon1/src/main/java/gitAThon1/Main.java
@@ -0,0 +1,13 @@
+package gitAThon1;
+
+//import gitAThon1.ChromeTests;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "howdy");
+	//	ChromeTests ct = new ChromeTests();
+	}
+
+}
---
+++ repository/MavenProject4/src/main/java/mavenProject4/Main.java
@@ -0,0 +1,11 @@
+package mavenProject4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "howdy");
+		ChromeTests ct = new ChromeTests();
+	}
+
+}
